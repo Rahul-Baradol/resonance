34d0229d38aded0d5cba5d9d5caee17b193e54be
Rahul-Baradol <rahul.baradol.14@gmail.com>
2025-03-22 23:52:30 +0530

    ping log

--- resonance-backend/src/main/java/com/resonance/resonancebackend/controller/Health.java
+++ resonance-backend/src/main/java/com/resonance/resonancebackend/controller/Health.java
@@ -1,14 +1,17 @@
 package com.resonance.resonancebackend.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class Health {
 
     @GetMapping("/health")
     public ResponseEntity<Void> ping() {
+        log.debug("Ping!");
         return ResponseEntity.noContent().build();
     }
 
