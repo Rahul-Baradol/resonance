2db69c4a977adffaaa15a736cc04fb9521735924
Rahul Baradol <rahul.baradol.14@gmail.com>
2025-11-23 14:19:04 +0530

    middleware

---
+++ resonance-frontend/middleware.ts
@@ -0,0 +1,14 @@
+// middleware.js or middleware.ts
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  res.headers.set('Access-Control-Allow-Origin', '*');
+  res.headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  return res;
+}
+
+export const config = {
+  matcher: '/api/playlists', // apply to all API routes
+};
